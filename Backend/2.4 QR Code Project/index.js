7fa78462d8f61d93c42631ab05debc0efa0494a4
ajisrael <43039187+ajisrael@users.noreply.github.com>
2023-10-12 21:06:46 -0400

    f   - completed qr code challenge

--- Backend/2.4 QR Code Project/index.js
+++ Backend/2.4 QR Code Project/index.js
@@ -3,3 +3,33 @@
 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 3. Create a txt file to save the user input using the native fs node module.
 */
+import inquirer from 'inquirer';
+import qr from 'qr-image';
+import fs from 'fs';
+
+inquirer
+  .prompt([
+    {
+      message: "Type in your URL: ",
+      name: "URL",
+    },
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    const qrImage = qr.image(url);
+
+    qrImage.pipe(fs.createWriteStream("qr_img.png"));
+
+    fs.writeFile("url.txt", url, (err) => {
+      if (err) throw err;
+      console.log("URL written successfully!");
+    });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      console.log("Prompt couldn't be rendered in the current environment");
+    } else {
+      console.log("Something else went wrong");
+    }
+    throw error;
+  });
